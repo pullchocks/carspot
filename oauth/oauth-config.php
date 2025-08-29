5a008d895c5bf18ec1fe81ab005b474bd95166a4
pullchocks <160859472+pullchocks@users.noreply.github.com>
2025-08-28 20:40:18 -0500

    API Swap

--- oauth/oauth-config.php
+++ oauth/oauth-config.php
@@ -1,7 +1,7 @@
 <?php
 // GTA World OAuth Configuration for Car Spot
 define('CLIENT_ID', '72');
-define('CLIENT_SECRET', 'WPy568Q029DHCfGfuhbVqjQqzSm29JAVCMQeqbNh');
+define('CLIENT_SECRET', 'm6oBgUkqNlX8ex8Pf4o2chlCluAPR5nsfitdnxMM');
 define('REDIRECT_URI', 'https://carspot.site/oauth/callback');
 define('AUTH_URL', 'https://ucp.gta.world/oauth/authorize');
 define('TOKEN_URL', 'https://ucp.gta.world/oauth/token');
