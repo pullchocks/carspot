202d778d982266084ec58d12e3048299d6857184
pullchocks <160859472+pullchocks@users.noreply.github.com>
2025-08-30 18:28:35 -0500

    CORS Fix For Webhooks

--- api/webhooks.php
+++ api/webhooks.php
@@ -1,16 +1,5 @@
 <?php
-// Add CORS headers for cross-origin requests
-header('Access-Control-Allow-Origin: https://carspot.site');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header('Access-Control-Allow-Credentials: true');
-
-// Handle preflight OPTIONS request
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit(0);
-}
-
+// Set content type for JSON responses
 header('Content-Type: application/json');
 
 require_once 'database.php';
