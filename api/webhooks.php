f9ae062d0fad0deebb93b9817ad78ee6859bd164
pullchocks <160859472+pullchocks@users.noreply.github.com>
2025-08-30 17:31:12 -0500

    webhooks.php fix

--- api/webhooks.php
+++ api/webhooks.php
@@ -3,7 +3,7 @@
 header('Access-Control-Allow-Origin: https://carspot.site');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header('Access-Control-Allow-Credentials: true);
+header('Access-Control-Allow-Credentials: true');
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
